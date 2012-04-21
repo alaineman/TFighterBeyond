27e27dccc35020703f8ac7abb4f329ae27d213d8
Daniel987600 <jackarno@gmail.com>
2012-04-21 02:10:22 -0700

    Update src/com/zalgoproductions/util/Alchemy.java

--- src/com/zalgoproductions/util/Alchemy.java
+++ src/com/zalgoproductions/util/Alchemy.java
@@ -18,67 +18,71 @@ public class Alchemy {
 	private static final int NATURE_RUNE = 561;
 	private static final int FIRE_RUNE = 554;
 
-	public static final Filter<Item> ALCH_FILTER =
-			new Filter<Item>() {
-				public boolean accept(Item item) {
-					for( Item i : Inventory.getItems()) {
-						for ( String s : alchNames) {
-							if (i.getWidgetChild() != null && i.getId() != -1 && i.getName().toLowerCase().contains(s)) {
-								return true;
-							}
-						}
-						for ( int n : alchIds) {
-							if( n == i.getId()) {
-								return true;
-							}
-						}
-					}
-					return false;
+	public static final Filter<Item> ALCH_FILTER = new Filter<Item>() {
+
+		@Override
+		public boolean accept(Item i) {
+			if (i.getWidgetChild() == null || i.getId() == -1) {
+				return false;
+			}
+			String itemName = i.getName().toLowerCase();
+			for (String s : alchNames) {
+				if (itemName.contains(s)) {
+					return true;
 				}
-			};
+			}
+			for (int n : alchIds) {
+				if (n == i.getId()) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+	};
 
 	public static void setAlchIds(int[] ids) {
 		alchIds = ids;
 	}
-	
+
 	public static void setAlchNames(String[] names) {
 		alchNames = names;
 	}
-	
+
 	public static boolean canAlch() {
 		return canHighAlch() || canLowAlch();
 	}
-	
+
 	private static boolean canHighAlch() {
-		return Inventory.getCount(true, NATURE_RUNE) >= 1 && (Inventory.getCount(true, FIRE_RUNE) >= 5 ||
-				Attacking.playerEquipment[3].getName().toLowerCase().contains("fire") ||
-				Attacking.playerEquipment[3].getName().toLowerCase().contains("steam") ||
-				Attacking.playerEquipment[3].getName().toLowerCase().contains("lava")) &&
-				Skills.getLevel(Skills.MAGIC) >= 55;
-	}
-	
-	private static boolean canLowAlch() {
-		return Inventory.getCount(true, NATURE_RUNE) >= 1 && (Inventory.getCount(true, FIRE_RUNE) >= 3 ||
-				Attacking.playerEquipment[3].getName().toLowerCase().contains("fire") ||
-				Attacking.playerEquipment[3].getName().toLowerCase().contains("steam") ||
-				Attacking.playerEquipment[3].getName().toLowerCase().contains("lava")) &&
-				Skills.getLevel(Skills.MAGIC) >= 21;
+		return Inventory.getCount(true, NATURE_RUNE) >= 1
+				&& Skills.getLevel(Skills.MAGIC) >= 55
+				&& (Inventory.getCount(true, FIRE_RUNE) >= 5
+					|| Attacking.playerEquipment[3].getName().toLowerCase().contains("fire")
+					|| Attacking.playerEquipment[3].getName().toLowerCase().contains("steam")
+					|| Attacking.playerEquipment[3].getName().toLowerCase().contains("lava"));
 	}
 
-	public static void alch(Item i) {
-		if (i != null) {
+	private static boolean canLowAlch() {
+		return Inventory.getCount(true, NATURE_RUNE) >= 1
+				&& Skills.getLevel(Skills.MAGIC) >= 21
+				&& (Inventory.getCount(true, FIRE_RUNE) >= 3
+					|| Attacking.playerEquipment[3].getName().toLowerCase().contains("fire")
+					|| Attacking.playerEquipment[3].getName().toLowerCase().contains("steam")
+					|| Attacking.playerEquipment[3].getName().toLowerCase().contains("lava"));
+	}
+
+	public static boolean alch(Item i) {
+		if (i != null && canLowAlch()) {
 			Tabs.MAGIC.open();
-			Widget modernBook = Widgets.get(192);
-			if(!modernBook.validate())
-				return;
-			if(canHighAlch()) {
-				modernBook.getChild(59).interact("Cast");
+			if (Sleeping.waitForTabChange(Tabs.MAGIC)) {
+				Widget modernBook = Widgets.get(192);
+				if (!modernBook.validate()) {  return false;  }
+				final int widgetChildID = canHighAlch() ? 59 : 38;
+				return modernBook.getChild(widgetChildID).interact("Cast")
+						&& Sleeping.waitForTabChange(Tabs.INVENTORY)
+						&& i.getWidgetChild().interact("Cast");
 			}
-			else if(canLowAlch()) {
-				modernBook.getChild(38).interact("Cast");
-			}
-			Time.sleep(Random.nextInt(150, 500));
-			i.getWidgetChild().interact("Cast");
 		}
+		return false;
 	}
 }
